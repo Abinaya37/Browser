045c551d2532df35e3b3de95bb26b8722f067883
Abinaya37 <abinaya30091998@gmail>
2022-11-27 08:44:48 +0530

    Browser launch

---
+++ Browser_Launch.java
@@ -0,0 +1,29 @@
+package com.sample.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Browser_Launch {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Lenovo\\eclipse-workspace\\Selenium_Project\\Driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/");
+
+		String title = driver.getTitle();
+		System.out.println(title);
+
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+
+		driver.close();
+
+		driver.quit();
+
+	}
+
+}
